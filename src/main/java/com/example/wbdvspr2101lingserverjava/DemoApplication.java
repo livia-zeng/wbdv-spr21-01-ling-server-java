f2c34cb6863a658630f7fdaf7bf23015252f395e
livia <ainezling@gmail.com>
2021-02-01 11:53:10 -0800

    first commit

---
+++ src/main/java/com/example/wbdvspr2101lingserverjava/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wbdvspr2101lingserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wbdvspr2101lingserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvspr2101lingserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
